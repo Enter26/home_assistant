bc226c550f57ad2788261bfa2c769e618f01865a
Enter26 <39347145+Enter26@users.noreply.github.com>
2026-02-21 13:26:14 +0100

    Update initial_sql_setup.sql

--- my_webpage/initial_sql_setup.sql
+++ my_webpage/initial_sql_setup.sql
@@ -13,4 +13,23 @@ TABLESPACE pg_default;
 
 ALTER TABLE public.logs
     OWNER to postgres;
---Message Table
+
+-- Table: public.web_messages
+
+-- DROP TABLE public.web_messages;
+
+CREATE TABLE IF NOT EXISTS public.web_messages
+(
+    "Id" integer NOT NULL DEFAULT nextval('"web_messages_Id_seq"'::regclass),
+    date timestamp without time zone,
+    language text COLLATE pg_catalog."default",
+    email text COLLATE pg_catalog."default",
+    message text COLLATE pg_catalog."default",
+    status text COLLATE pg_catalog."default",
+    CONSTRAINT web_messages_pkey PRIMARY KEY ("Id")
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.web_messages
+    OWNER to postgres;
