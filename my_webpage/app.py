35d4f708e006791da861421f0677f87445905a7e
Enter26 <39347145+Enter26@users.noreply.github.com>
2026-02-21 13:32:19 +0100

    Update app.py

--- my_webpage/app.py
+++ my_webpage/app.py
@@ -91,4 +91,4 @@ def contactde():
         return render_template('contactde.html', results="results")
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5013,debug=True)
+    app.run(host="0.0.0.0", port=8080,debug=True)
