082633beb8803e6159fb809176675796775f1b19
Enter26 <39347145+Enter26@users.noreply.github.com>
2026-02-21 13:23:28 +0100

    Update initial_sql_setup.sql

--- my_webpage/initial_sql_setup.sql
+++ my_webpage/initial_sql_setup.sql
@@ -1,4 +1,16 @@
 --Initial SQL setup for store messages
 --Log Table
+CREATE TABLE IF NOT EXISTS public.logs
+(
+    "Id" integer NOT NULL DEFAULT nextval('"logs_Id_seq"'::regclass),
+    date timestamp without time zone,
+    app text COLLATE pg_catalog."default",
+    description text COLLATE pg_catalog."default",
+    status text COLLATE pg_catalog."default"
+)
 
+TABLESPACE pg_default;
+
+ALTER TABLE public.logs
+    OWNER to postgres;
 --Message Table
